f323c895038333d56f4d0e8c7b2c7501897911ee
Kananek T <info.dvgamer@gmail.com>
2018-12-13 21:50:50 +0700

    package update and waka schema

--- mongo/db-touno/wakatime.js
+++ mongo/db-touno/wakatime.js
@@ -4,21 +4,41 @@ module.exports = [
     id: 'WakaSummaries',
     name: 'db-wakatime',
     schema: mongoose.Schema({
-      name: String,
-      date: Date,
+      name: { index: true, type: String },
+      type: { index: true, type: String },
+      date: { index: true, type: Date },
+      total: {
+        hours: Number,
+        minutes: Number,
+        seconds: Number
+      },
+      total_digital: String,
       total_text: String,
       total_seconds: Number,
-      type: String
+      created: Date,
+      updated: Date
     })
   },
   {
-    id: 'WakaHeartbeat',
-    name: 'db-waka_heartbeat',
+    id: 'WakaProjects',
+    name: 'db-wakatime-project',
     schema: mongoose.Schema({
-      entity: String,
-      id: String,
-      time: Number,
-      type: String
+      project: { index: true, type: String },
+      name: { index: true, type: String },
+      type: { index: true, type: String },
+      date: { index: true, type: Date },
+      storage: { index: true, type: String },
+      repositories: String,
+      total: {
+        hours: Number,
+        minutes: Number,
+        seconds: Number
+      },
+      total_digital: String,
+      total_text: String,
+      total_seconds: Number,
+      created: Date,
+      updated: Date
     })
   }
 ]
