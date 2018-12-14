9607691fa94a20ca4be8233c81c88b4937f5666a
Kananek Thongkam <info.dvgamer@gmail.com>
2018-12-14 05:45:17 +0000

    OAuth Schema updated.

--- mongo/db-touno/app.js
+++ mongo/db-touno/app.js
@@ -27,22 +27,23 @@ module.exports = [
     id: 'OAuth',
     name: 'oauth-3rd',
     schema: mongoose.Schema({
-      name: String,
-      client: String,
+      name: { type: String, index: true },
+      key: { type: String, index: true },
+      type: { type: String, index: true },
+      scope: String,
       state: String,
+      client: {
+        id: String,
+        secret: String
+      },
+      auth: {
+        tokenHost: String,
+        tokenPath: String,
+        authorizePath: String
+      },
       token: Object,
       updated: { type: Date, index: true },
-      created: { type: Date, index: true
-      }
-    })
-  },
-  {
-    id: 'Account',
-    name: 'oauth-account',
-    schema: mongoose.Schema({
-      username: String,
-      token: String,
-      active: Boolean
+      created: { type: Date, index: true }
     })
   },
   {
