3f6bd87efa1973942fe48fab2d6a4df4b6eb3f9e
Kananek T <info.dvgamer@gmail.com>
2019-01-20 01:30:51 +0700

    add website

--- mongo/index.js
+++ mongo/index.js
@@ -4,5 +4,6 @@ moment.tz.setDefault(process.env.TZ || 'Asia/Bangkok')
 module.exports = {
   touno: require('./db-touno'),
   crypto: require('./db-crypto'),
-  opensource: require('./db-opensource')
+  opensource: require('./db-opensource'),
+  website: require('./db-website')
 }
