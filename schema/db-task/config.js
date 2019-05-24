adbea6e30a40060e60c0e553216baf255017b85b
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-24 18:21:59 +0700

    update config key-value.

--- schema/db-task/config.js
+++ schema/db-task/config.js
@@ -1,4 +1,16 @@
+const { Mixed } = require('../../type')
+
 module.exports = [
+  {
+    id: 'Config',
+    name: 'config',
+    schema: {
+      scope: { index: true, type: String },
+      key: { index: true, type: String },
+      value: Mixed,
+      created: { index: true, type: Date, Default: Date.now }
+    }
+  },
   {
     id: 'ConfigTask',
     name: 'config-tasks',
--- schema/db-task/index.js
+++ schema/db-task/index.js
@@ -2,5 +2,5 @@ const mongo = require('../../index')('db-task')
 
 mongo.set(require('./bnk48'))
 mongo.set(require('./pokemon-go'))
-mongo.set(require('./crontab'))
+mongo.set(require('./config'))
 module.exports = mongo
