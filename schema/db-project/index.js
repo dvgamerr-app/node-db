0f989c4eeaffd6456e5e121c8082d91e63f6be0c
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-26 23:42:54 +0700

    addon linebot

--- schema/db-project/index.js
+++ schema/db-project/index.js
@@ -2,4 +2,5 @@ const mongo = require('../../index')('db-project')
 
 mongo.set(require('./exhentai'))
 mongo.set(require('./rotomu'))
+mongo.set(require('./line-bot'))
 module.exports = mongo
---
+++ schema/db-project/line-bot.js
@@ -0,0 +1,27 @@
+const { Mixed } = require('../../type')
+
+module.exports = [
+  {
+    id: 'LineBot',
+    name: 'line-bot',
+    schema: {
+      botname: { type: String, index: true, unique: true, require: true },
+      name: String,
+      accesstoken: String,
+      secret: String,
+      options: Object,
+      created: { type: Date, index: true, default: Date.now }
+    }
+  },
+  {
+    id: 'LineNotify',
+    name: 'line-notify',
+    schema: {
+      botname: { type: String, index: true, require: true, Ref: 'LineBot' },
+      payload: { type: Mixed, default: null },
+      notify: { type: Boolean, index: true, default: false },
+      schedule: { type: Date, index: true, default: null },
+      created: { type: Date, index: true, default: Date.now }
+    }
+  }
+]
