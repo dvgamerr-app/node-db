76fd4885a1109ddd13661922f6495c4891e2e922
Kananek Thongkam <info.dvgamer@gmail.com>
2019-07-18 23:44:00 +0700

    add team db.

--- schema/db-touno-io/index.js
+++ schema/db-touno-io/index.js
@@ -2,4 +2,5 @@ const mongo = require('../../index')('db-touno-io')
 
 mongo.set(require('./donate'))
 mongo.set(require('./resume'))
+mongo.set(require('./team-dev'))
 module.exports = mongo
---
+++ schema/db-touno-io/team-dev.js
@@ -0,0 +1,63 @@
+const { ObjectId } = require('../../type')
+
+module.exports = [
+  {
+    id: 'Account',
+    name: 'team-account',
+    schema: {
+      username: { type: String, index: true, require: true, unique: true },
+      pwd: { type: String, index: true, require: true },
+      fullname: { type: String, index: true },
+      email: { type: String, index: true },
+      level: { type: Number, index: true },
+      permission: { type: Array, default: null },
+      facebook: { type: Object, default: null },
+      line: { type: Object, default: null },
+      enabled: { type: Boolean, index: true, default: true },
+      lasted: { type: Date, index: true, default: Date.now },
+      updated: { type: Date, index: true, default: Date.now },
+      created: { type: Date, index: true, default: Date.now }
+    }
+  },
+  {
+    id: 'Todo',
+    name: 'team-todo',
+    schema: {
+      owner: {
+        _id: { type: ObjectId, index: true },
+        fullname: String,
+        email: String
+      },
+      title: { type: String, index: true },
+      description: String,
+      project: { type: String, index: true },
+      duedate: { type: Date, index: true },
+      priority: { type: Number, index: true },
+      assign: [
+        {
+          _id: { type: ObjectId, index: true },
+          fullname: String,
+          email: String
+        }
+      ],
+      tags: [ String ],
+      status: { type: Number, index: true, default: 0 },
+      private: { type: Boolean, index: true, default: false  },
+      deleted: { type: Boolean, index: true, default: false  },
+      updated: { type: Date, index: true, default: Date.now },
+      created: { type: Date, index: true, default: Date.now },
+    }
+  },
+  {
+    id: 'Comment',
+    name: 'team-comment',
+    schema: {
+      todo: { type: ObjectId, index: true },
+      owner: { type: ObjectId, index: true },
+      description: String,
+      deleted: { type: Boolean, index: true },
+      updated: { type: Date, index: true, default: Date.now },
+      created: { type: Date, index: true, default: Date.now },
+    }
+  }
+]
