a73a9e3bf9a93f16e56ed7f7426b31a748376b0f
Kananek Thongkam <info.dvgamer@gmail.com>
2019-09-18 15:20:49 +0700

    remove old version.

--- mongo/index.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment-timezone')
-moment.tz.setDefault(process.env.TZ || 'Asia/Bangkok')
-
-module.exports = {
-  touno: require('./db-touno'),
-  crypto: require('./db-crypto'),
-  opensource: require('./db-opensource'),
-  website: require('./db-website')
-}
