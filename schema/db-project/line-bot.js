94ce47dbaa90cc974198bd910088e6a8a90aee7f
Kananek Thongkam <info.dvgamer@gmail.com>
2019-12-08 23:27:14 +0700

    fixed data in line-bot

--- schema/db-project/line-bot.js
+++ schema/db-project/line-bot.js
@@ -6,8 +6,6 @@ module.exports = [
     name: 'line-notify',
     schema: {
       endpoint: { type: String, require: true },
-      method: { type: String, require: true },
-      botname: { type: String, index: true },
       payload: { type: Mixed, default: null },
       notify: { type: Boolean, index: true, default: false },
       schedule: { type: Date, index: true, default: Date.now }
