75ab38dfed0a005066336597af4037cc61545afb
Kananek Thongkam <info.dvgamer@gmail.com>
2020-01-08 22:49:21 +0700

    fixed db-logging remove.

--- schema/index.js
+++ schema/index.js
@@ -2,7 +2,6 @@ const moment = require('moment-timezone')
 moment.tz.setDefault(process.env.TZ || 'Asia/Bangkok')
 
 module.exports = {
-  logging: require('./db-logging'),
   myself: require('./db-myself'),
   oauth: require('./db-oauth'),
   project: require('./db-project'),
