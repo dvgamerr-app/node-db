1106a30410d17d077f51300ac595628fe7b4e254
Kananek Thongkam <info.dvgamer@gmail.com>
2021-04-12 16:48:28 +0700

    add method state.

--- task/index.js
+++ task/index.js
@@ -5,6 +5,17 @@ const createCronJob = ({ name }, manual = false, Task, TaskHistory) => {
   let cron = null
   let state = 2
   return {
+    async getState () {
+      const job = await Task.findOne({ name }, 'started')
+      if (!cron) return { working: job.started, running: null, state: 'N/A' }
+
+      return {
+        working: job.started,
+        running: cron.state > 0,
+        error: cron.exited !== 0,
+        state: cron.exited !== 0 ? 'FATAL' :cron.state === 0 ? 'COMPLATED' : cron.state === 1 ? 'INITIALIZE' : cron.state === 2 ? 'STARTING' : 'PROCESSING'
+      }
+    },
     async isRunning () {
       if (!cron) return false
       const job = await Task.findOne({ name })
@@ -16,15 +27,19 @@ const createCronJob = ({ name }, manual = false, Task, TaskHistory) => {
     },
     async start () {
       if (!cron) return
+      cron.state = 2
       await TaskHistory.updateOne({ _id: cron._id }, { $set: { state: 2, updated: new Date() } })
     },
     async processing () {
       if (!cron) return
       state++
+      cron.state = state
       await TaskHistory.updateOne({ _id: cron._id }, { $set: { state, updated: new Date() } })
     },
     async finish (exited = 1) {
       if (!cron) return
+      cron.state = 0
+      cron.exited = exited
       await Task.updateOne({ name }, { $set: { started: false }})
       await TaskHistory.updateOne({ _id: cron._id }, { $set: { state: 0, updated: new Date(), exited } })
     },
@@ -44,7 +59,7 @@ module.exports = {
   },
   load: async (name, manual) => {
     await task.open()
-    
+
     const { Task, TaskHistory } = task.get()
     const job = await Task.findOne({ name }, '_id')
     if (!job) throw new Error('task-job is not setting.')
